1dafc3b19d81e550f76cf13c738eb88d72153c16
unknown <administrator@mgt.winhpc.qhu>
2018-07-27 16:06:53 +0800

    findPath

--- findPath.py
+++ findPath.py
@@ -1,1 +1,11 @@
 import os
+import netCDF4 as nc
+import matplotlib as plt
+
+prefix1 = '../TRR_Data/north/'
+prefix2 = '../TRR_Data/east/'
+suffix = '.nc'
+
+ncobj = nc.Dataset(prefix1+'1979'+suffix)
+print(ncobj.variables['longitude'][:])
+print(ncobj.variables['latitude'][:])
